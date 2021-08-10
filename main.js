4add605b8e4fecdec683e340d5efc4b4f3a3ac4d
anaximeno <anaximenobrito@gmail.com>
2021-08-10 17:41:35 -0100

    update: a estrutura do código foi melhorada

--- main.js
+++ main.js
@@ -12,9 +12,17 @@ const RESIZE_SUB_FACTOR = 30;
 let ctx;
 
 
-function canvasSize() {
-    let size = window.outerWidth > (MAX_CANVAS_SIZE + RESIZE_SUB_FACTOR) ? 
-        MAX_CANVAS_SIZE : (window.outerWidth - RESIZE_SUB_FACTOR);
+function canvasSize(maxsize = MAX_CANVAS_SIZE)
+{
+    let size = window.outerWidth > (maxsize + RESIZE_SUB_FACTOR) ?
+        maxsize : (window.outerWidth - RESIZE_SUB_FACTOR);
+    return size;
+}
+
+
+function ctxSize(maxsize = MAX_CTX_SIZE)
+{
+    let size = window.outerWidth > 280 ? maxsize : 15;
     return size;
 }
 
@@ -31,14 +39,13 @@ function resizeCanvas()
     // Get the canvas element
     const canvas = document.getElementById('draw-canvas');
     // Set the width and the height to the better possible size
-    canvas.width = canvas.height = canvasSize();
-    
+    canvas.width = canvas.height = canvasSize(MAX_CANVAS_SIZE);
     ctx = canvas.getContext('2d');
     ctx.strokeStyle = 'white';
     ctx.fillStyle = 'white';
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
-    ctx.lineWidth = window.innerWidth > 280 ? MAX_CTX_SIZE : 15;
+    ctx.lineWidth = ctxSize(MAX_CTX_SIZE);
 }
 
 
@@ -57,14 +64,13 @@ function prepareCanvas()
     canvas.addEventListener('mouseout', async () => {
         let wasDrawing = drawing;
         drawing = false;
-
         await sleep(1100);
-        if (wasDrawing && !drawing && isModelLoaded) predict();
+        if (wasDrawing && !drawing && isModelLoaded)
+            predict();
     });
     canvas.addEventListener('mousemove', (e) => {
         if (!drawing)
             return ;
-
         ctx.beginPath();
         ctx.moveTo(lastPosition.x, lastPosition.y);
         ctx.lineTo(e.offsetX, e.offsetY);
@@ -73,7 +79,6 @@ function prepareCanvas()
     });
     canvas.addEventListener('mouseup', async () => {
         drawing = false;
-
         await sleep(650);
         if (!drawing && isModelLoaded)
             predict();
@@ -91,7 +96,7 @@ function prepareCanvas()
     });
     canvas.addEventListener('touchmove', (e) => {
         e.preventDefault();
-        
+
         // If not drawing stop the execution of this funtion here.
         if (!drawing)
             return ;
@@ -146,7 +151,7 @@ async function loadModel()
     model = await tf.loadLayersModel(MODEL_PATH);
     isModelLoaded = true;
 
-    
+
     // Uncomment the line below if you want to see output on your browser console.
     // console.log("The model was loaded successfully!");
 
@@ -163,18 +168,14 @@ async function predict()
 
     const p = document.getElementById('predict-output');
     p.innerText = 'Predicting...';
-    
+
     tf.engine().startScope();
     // Get the canvas element with the drawing
     const canvas = document.getElementById('draw-canvas');
     // Aplicate the preprocessing transformations for being able to be a valid input to the model
     const toPredict = tf.browser.fromPixels(canvas)
-        .resizeBilinear([IMAGE_SIZE, IMAGE_SIZE])
-        .mean(2)
-        .expandDims()
-        .expandDims(3)
-        .toFloat()
-        .div(255.0);
+    	.resizeBilinear([IMAGE_SIZE, IMAGE_SIZE])
+	.mean(2).expandDims().expandDims(3).toFloat().div(255.0);
     // Predict the data and return an array with the probability of all possible outputs
     const prediction = model.predict(toPredict).dataSync();
     // Set the prediction to the output with the max probability (greater value) and shows it to the user
@@ -186,22 +187,22 @@ async function predict()
 /** Automatic call to function init */
 (function init() {
     // Prepares the canvas to be used
-    prepareCanvas();  
-    
+    prepareCanvas();
+
     const p = document.getElementById('predict-output');
-    const pipe = document.getElementById('pipeline'); 
-    p.style.width = pipe.style.width = `${canvasSize()}px`;
+    const pipe = document.getElementById('pipeline');
+    p.style.width = pipe.style.width = `${canvasSize(MAX_CANVAS_SIZE)}px`;
 
     // Create the clear button along with its event
     createButton('Clear', '#pipeline', 'clear-btn', () => {
-        ctx.clearRect(0, 0, canvasSize(), canvasSize());
+        ctx.clearRect(0, 0, canvasSize(MAX_CANVAS_SIZE), canvasSize(MAX_CANVAS_SIZE));
         if (isModelLoaded)
             p.innerHTML = 'Draw any digit between <strong>0</strong> to <strong>9</strong>.';
     });
 
     /** When resizing the window some other elements must be resized also */
     window.addEventListener('resize', () => {
-        p.style.width = pipe.style.width = `${canvasSize()}px`;
+        p.style.width = pipe.style.width = `${canvasSize(MAX_CANVAS_SIZE)}px`;
         resizeCanvas();
         if (isModelLoaded)
             p.innerHTML = 'Draw any digit between <strong>0</strong> to <strong>9</strong>.';
