b146fe444bd1d92d47ab369b6ebffabe6a32a129
anaximeno <anaximenobrito@gmail.com>
2022-01-11 14:00:10 -0100

    made some refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
+import { Logger } from './common';
+import { OutputLabel } from './common';
+import { Button} from './common';
 import { Canvas } from './canvas';
-import { Logger, OutputLabel, Button} from './common';
 import { Model } from './model'
 import { App } from "./app";
 
 
-const outLabelDMSG_P1 = "<div id='output-text'>Draw any digit between <strong>";
-const outLabelDMSG_P2 = "0</strong> to <strong>9</strong><\div>";
+const DEBUG_MODE_ENABLED = true
+const DefaultMsgP01 = "<div id='output-text'>Draw any digit between <strong>";
+const DefaultMsgP02 = "0</strong> to <strong>9</strong><\div>";
 
 
 (function (build: Function) {    
     const app = build();
     app.run();
 })(() => {
-    const logger = new Logger(false);
-    const outputLabel = new OutputLabel('output', outLabelDMSG_P1+outLabelDMSG_P2);
+    const logger = new Logger(DEBUG_MODE_ENABLED);
+    const outputLabel = new OutputLabel('output', DefaultMsgP01 + DefaultMsgP02);
     const eraseButton = new Button('erase-btn', 'Clear all drawings', 'Please wait');
     const canvas = new Canvas('draw-canvas', { width: 400, height: 400 }, 22);
     const model = new Model('./data/compiled/model.json', canvas, eraseButton, outputLabel, logger);
--- static/main.js
+++ static/main.js
@@ -1,17 +1,20 @@
 "use strict";
 export const __esModule = true;
+import { Logger } from "./common.js";
+import { OutputLabel } from "./common.js";
+import { Button } from "./common.js";
 import { Canvas } from "./canvas.js";
-import { Logger, OutputLabel, Button } from "./common.js";
 import { Model } from "./model.js";
 import { App } from "./app.js";
-var outLabelDMSG_P1 = "<div id='output-text'>Draw any digit between <strong>";
-var outLabelDMSG_P2 = "0</strong> to <strong>9</strong><\div>";
+var DEBUG_MODE_ENABLED = true;
+var DefaultMsgP01 = "<div id='output-text'>Draw any digit between <strong>";
+var DefaultMsgP02 = "0</strong> to <strong>9</strong><\div>";
 (function (build) {
     var app = build();
     app.run();
 })(function () {
-    var logger = new Logger(false);
-    var outputLabel = new OutputLabel('output', outLabelDMSG_P1 + outLabelDMSG_P2);
+    var logger = new Logger(DEBUG_MODE_ENABLED);
+    var outputLabel = new OutputLabel('output', DefaultMsgP01 + DefaultMsgP02);
     var eraseButton = new Button('erase-btn', 'Clear all drawings', 'Please wait');
     var canvas = new Canvas('draw-canvas', { width: 400, height: 400 }, 22);
     var model = new Model('./data/compiled/model.json', canvas, eraseButton, outputLabel, logger);
